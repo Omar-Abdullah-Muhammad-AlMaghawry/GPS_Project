ebcfbcd4a1a40a29f2cc034f88e6e899d8724be2
OmarAbdullah <1700884@eng.asu.edu.eg>
2021-05-27 21:44:29 +0200

    uart2

--- uart.h
+++ uart.h
@@ -2,7 +2,7 @@
 #define uart
 #include "stdint.h"
 #include "tm4c123gh6pm.h"
-#define UARTTYPE 0;
+#define UARTTYPE 2
 void UART0_init(void);
 uint8_t UART_recieve(void);
 void UART_sent(uint8_t data);
