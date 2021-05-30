ccf98a264d609a255e959757e7db47d5c87c89a7
OmarAbdullah <1700884@eng.asu.edu.eg>
2021-05-30 22:03:42 +0200

    MORE portable uart_ FOR STOPBITS & NOOFBITS

--- main.c
+++ main.c
@@ -3,19 +3,20 @@
 	
 int main(void){
 	uint8_t test=0;
-	UartConfig confUart={UART_0,PORTA,1,8,9600};
-		lcd_init();
+	UartConfig confUart={UART_2,PORTD,oneStop,eightBits,9600};
+	lcd_init();
 	lcd_select_line(1);
 	lcd_write_line("f 00");
 	UART_init(&confUart); 	
 	lcd_select_line(2);
 	lcd_write_line("f 01");
 	while(1){	
-		test =UART_recieve(&confUart);
+		test = UART_recieve(&confUart);
+		UART_sent(test,&confUart);
 		lcd_select_line(3);
 		lcd_write_char(test);
-		UART_sent(test,&confUart);
+		
 	}
 	return 0 ;
 }
-void SystemInit(){}
+void SystemInit(){main();}
